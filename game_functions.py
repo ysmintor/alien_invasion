261ad6155411c3f3f832ed9cc3335ba8180fd2de
YorkYu <ysmintor@gmail.com>
2017-01-12 16:18:20 +0800

    hide and show mouse appropriately

--- game_functions.py
+++ game_functions.py
@@ -52,6 +52,8 @@ def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bul
     """Start a new game when the player clicks Play."""
     button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
     if button_clicked and not stats.game_active:
+        # Hide the mouse cursor.
+        pygame.mouse.set_visible(False)
         # Reset the game statistics.
         stats.reset_stats()
         stats.game_active = True
@@ -174,6 +176,7 @@ def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
         sleep(0.5)
     else:
         stats.game_active = False
+        pygame.mouse.set_visible(True)
 
 
 def check_alien_bottom(ai_settings, stats, screen, ship, aliens, bullets):
