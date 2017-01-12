8f17aeecb4a866aa0716d4a76c56943d53da2e11
YorkYu <ysmintor@gmail.com>
2017-01-12 15:48:36 +0800

    reset game added

--- game_stats.py
+++ game_stats.py
@@ -5,8 +5,8 @@ class GameStats():
         """Initialize statistics."""
         self.ai_settings = ai_settings
         self.reset_stats()
-        # Stat Alien Invasion in an active state.
-        self.game_active = True
+        # Stat Alien Invasion in an inactive state.
+        self.game_active = False
 
     def reset_stats(self):
         """Initialize statistics that can change during the game."""
